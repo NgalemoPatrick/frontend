4c3b097c4ea24ab892e5569a739879dfb1ad54d5
Patrick Ngalemo <ngalemopatrick@outlook.com>
2023-09-12 00:38:21 -0500

    frontend

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
